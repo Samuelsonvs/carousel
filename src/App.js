55850b1e7fa0968fd0d825bd30d2aedd35fdc49c
Kadir Atalı <m.sametatali@gmail.com>
2022-11-09 21:27:46 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,98 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react';
 
-function App() {
+let imageArray = [
+  1,2,3,4,5,6
+]
+
+function Container() {
+  const renderChildrenView = (item ,index) =>{
+    return (
+      <div className='contentBox' key={index}>
+        <div className='cardBox'>
+          <div 
+            
+            className="imageStyle"/> 
+          <div className='fontBox'>
+            <p className='titleStyle'>{item}</p>
+          
+          </div>
+
+        </div>
+        
+     </div>
+    )
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="app">
+               <Carousel 
+          dataArray = {imageArray }
+          autoplay={true}
+          delay={1}
+          carouselPostWidth={'800px'} 
+          carouselPostHeight={150}
+          carouselPostMargin={10}>
+          {renderChildrenView}
+          </Carousel>
     </div>
   );
 }
 
-export default App;
+export default Container;
+
+const Carousel = (props) => {
+  const {dataArray,carouselPostMargin,carouselPostWidth,carouselPostHeight} = props
+  const [nowIndex, setNowIndex] = useState(0)
+  useEffect(() => {
+    if(props.autoplay){
+      const timer = setInterval(()=> {
+        changeImagePosition(1)
+   },props.delay *1000);
+   return () => clearInterval(timer)
+    }
+  }, [nowIndex])
+  
+  const conputedLeft = () =>{
+    console.log('nowIndex',nowIndex)
+    let leftSpan = parseInt(`${-nowIndex * parseInt(carouselPostWidth)}`)
+    return{
+      left: carouselPostWidth.toString().match(/[%vw]/) != null ? 
+      `calc(${leftSpan}% - ${carouselPostMargin*2*nowIndex}px)`: 
+      `${leftSpan - (carouselPostMargin*2*nowIndex)}px`
+    }
+  }
+  console.log(nowIndex)
+  const changeImagePosition = (index) =>{
+     // (1 + 1 + 3) % 3
+     setNowIndex(
+        (nowIndex + index +  dataArray.length) % dataArray.length
+     )
+  }
+  
+ 
+
+    return(
+      <div className='carouselContainer'>
+        <div className="carouselArea">
+          <div style={conputedLeft()} className="carouselPosts">
+            {dataArray.map((imgaeUrl,index)=>{
+              return(
+               <div 
+                 key={imgaeUrl} 
+                  style={{
+                    width:carouselPostWidth, 
+                    height:carouselPostHeight,  
+                    margin:`0px ${carouselPostMargin}px `,...props.carouselPostStyle}} 
+                  className="carouselPostBox">
+                  {props.children(imgaeUrl,index)}
+               </div>
+              )
+            })}
+        </div>
+      </div>
+      
+        <div onClick={()=>changeImagePosition(-1)} className="controlLeft">Prev</div>
+        <div onClick={()=>changeImagePosition(1)} className="controlRight">Next</div>
+      </div>
+    )
+  
+}
